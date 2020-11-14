1b9a940ee2b08f660f9e9a8c84075f372481ca57
Isuru Fernando <isuruf@gmail.com>
2020-11-14 10:17:07 -0600

    use autotools_clang_conda

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,36 +18,20 @@ case "$target_platform" in
         ./configure --prefix=$PREFIX $DISABLES
         ;;
     win-*)
-        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$RECIPE_DIR:$PATH"
-        export CC="$RECIPE_DIR/cl_wrapper.sh"
-        echo "$PATH"
-        $CC --version
-        export RANLIB=llvm-ranlib
-        export AS=llvm-as
-        export AR=llvm-ar
-        export LD=link
-        export CFLAGS="-MD -I$PREFIX/Library/include -O2"
-        export CXXFLAGS="-MD -I$PREFIX/Library/include -O2"
-        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib -no-undefined $PREFIX/Library/lib/pthreads.lib"
+        export LDFLAGS="$LDFLAGS -no-undefined $PREFIX/lib/pthreads.lib"
         # Skip failing tests that are skipped on Linux x86_64 and OSX, but not skipped on windows
         sed -i "s|SUBDIRS += x86||g" tests/hwloc/Makefile.am
         sed -i "s|-Xlinker --output-def -Xlinker .libs/libhwloc.def||g" hwloc/Makefile.am
         autoreconf -i
         chmod +x configure
-        chmod +x "$CC"
-        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" $DISABLES
+        ./configure --prefix="$PREFIX" --libdir="$PREFIX/lib" $DISABLES
+        patch_libtool
         make V=1
         ;;
 esac
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check -j${CPU_COUNT}
+  make check -j${CPU_COUNT}
 fi
 make install
-
-PROJECT=hwloc
-if [[ "$target_platform" == win-* ]]; then
-    LIBRARY_LIB=$PREFIX/Library/lib
-    mv "${LIBRARY_LIB}/${PROJECT}.dll.lib" "${LIBRARY_LIB}/${PROJECT}.lib"
-fi
