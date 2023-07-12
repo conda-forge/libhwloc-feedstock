72ae43044e16fb5a183608f62b422b6ff7918af5
Isuru Fernando <isuruf@gmail.com>
2023-07-11 23:35:32 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,8 @@ DISABLES="--disable-cairo --disable-opencl --disable-cuda --disable-nvml"
 DISABLES="$DISABLES --disable-gl --disable-libudev"
 
 chmod +x configure
-env
-echo "target_platform: $target_platform"
 
-case "$target_platform" in
+case "${target_platform:-${TARGET_PLATFORM}}" in
     osx-*)
         autoreconf -ivf
         ./configure --prefix=$PREFIX $DISABLES || (cat config.log; false)
